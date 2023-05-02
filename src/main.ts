bbdf00afd30b3cafa45a91edf71e304e017943cf
PhantasmicDev <oscar.phantasmic@protonmail.com>
2023-05-02 14:10:37 -0700

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { getVersionAndChangeset } from './Utils/version-handler'
+import { setupUnity } from './Utils/setup-unity'
 
 async function run(): Promise<void> {
-    const nameToGreet = core.getInput("name-to-greet")
-    console.log(`Hello ${nameToGreet}!`)
-} 
+    try {
+        let path = process.env.UNITY_PATH
+
+        if (!path) {
+            const [version, changeset] = await getVersionAndChangeset()
+            path = await setupUnity(version, changeset)
+            process.env.UNITY_PATH = path
+        }
+
+        const command = core.getInput("command")
+        exec.exec(`${path} ${command} -batchmode -nographics -username ${process.env.UNITY_USERNAME} -password ${process.env.UNITY_PASSWORD} -quit`)
+
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
+}
 
 run()
