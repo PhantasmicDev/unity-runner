eb5c926b7025a01664a337474060890058bb695c
Phantasmic Dev <74925183+PhantasmicDev@users.noreply.github.com>
2023-05-03 14:20:51 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function run(): Promise<void> {
 
         fs.writeFileSync(filePath, licenseContent)
 
-        exec.exec(`${path1} -batchmode -nographics -manualLicenseFile ${filePath} -logfile`)
+        exec.exec(`${path1} -batchmode -nographics -logfile`)
 
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
