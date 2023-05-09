f00ef5339b44f3f64485a4c972d7887d8209a12b
PhantasmicDev <oscar.phantasmic@protonmail.com>
2023-05-09 09:44:54 -0700

    refactored license activation

---
+++ src/Utils/activate-license.ts
@@ -0,0 +1,32 @@
+import * as os from 'os'
+import * as fs from 'fs'
+import * as exec from '@actions/exec'
+import * as path from 'path'
+
+export async function activateLicense(unityPath: string): Promise<void> {
+    if (hasActiveLicense()) {
+        return
+    }
+
+    const licenseContent = process.env.UNITY_LICENSE
+
+    if (!licenseContent){
+        return
+    }
+
+    const licenseFilePath = createLicenseFile(licenseContent)
+    await exec.exec(`${unityPath} -batchmode -manualLicenseFile ${licenseFilePath} -logfile`)
+}
+
+function hasActiveLicense(): boolean {
+    const licenseFilePath = `${os.homedir()}/.local/share/unity3d/Unity/Unity_lic.ulf`;
+    return fs.existsSync(licenseFilePath)
+}
+
+async function createLicenseFile(licenseContent: string): Promise<string> {
+    const tempFolder = process.env.RUNNER_TEMP || os.tmpdir()
+
+    const filePath = path.join(tempFolder, 'license.ulf')
+    fs.writeFileSync(filePath, licenseContent)
+    return filePath
+}
--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,43 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import { getVersionAndChangeset } from './Utils/version-handler'
 import { setupUnity } from './Utils/setup-unity'
+import { activateLicense } from './Utils/activate-license'
 import * as fs from 'fs'
 
 async function run(): Promise<void> {
     try {
-        let unityPath = process.env.UNITY_PATH
-
-        if (!unityPath) {
-            const [version, changeset] = await getVersionAndChangeset()
-            unityPath = await setupUnity(version, changeset)
-            
-            const envPath = process.env.GITHUB_ENV;
-
-            if (envPath) {
-                fs.appendFileSync(envPath, `UNITY_PATH=${unityPath}`);
-            }
-        }
-
+        const unityPath = await getPathToUnity()
+        
+        await activateLicense(unityPath)
+        
         const command = core.getInput("command")
+        const rawCommand = core.getBooleanInput("raw-command")
 
-        await exec.exec(`${unityPath} -batchmode -manualLicenseFile Unity_v2022.x.ulf -logfile`)
-
-        await exec.exec(`${unityPath} ${command}`)
-        console.log("Unity Command Done")
+        if (rawCommand){
+            await exec.exec(`${unityPath} ${rawCommand}`)
+        } else {
+            await exec.exec(`${unityPath} -batchmode -nographics ${command} -logFile -quit`)
+        }
 
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
     }
+}
+
+async function getPathToUnity() : Promise<string> {
+    let unityPath = process.env.UNITY_PATH
+
+    if (!unityPath) {
+        const [version, changeset] = await getVersionAndChangeset()
+        unityPath = await setupUnity(version, changeset)
+        
+        const envPath = process.env.GITHUB_ENV;
+
+        if (envPath) {
+            fs.appendFileSync(envPath, `UNITY_PATH=${unityPath}`);
+        }
+    }
+    return unityPath
 }
 
 run()
